7114f82a4536787d41dd44cc02368c693a68fbae
Justin Malamug <justin.malamug@capitalindex.com>
2023-06-11 04:21:54 +0800

    make date of birth nullable

--- Vidly/Vidly.Domain/Models/Customer.cs
+++ Vidly/Vidly.Domain/Models/Customer.cs
@@ -7,7 +7,7 @@ namespace Vidly.Domain.Models
         public int CustomerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public string MobileNumber { get; set; }
